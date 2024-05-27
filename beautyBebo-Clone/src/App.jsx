da33154528f74a50ed5a07995c2442b52990ebb1
SwethaS15 <swethas150905@gmail.com>
2024-05-27 15:37:24 +0530

    completed

---
+++ beautyBebo-Clone/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Brand from './components/Brand'
+import Navbar from './components/Navbar'
+import HeroMakeup from './components/HeroMakeup'
+import HeroSkin from './components/HeroSkin'
+function App() {
+  
+
+  return (
+    <>
+      <Brand/>
+      <Navbar/>
+      
+    </>
+  )
+}
+
+export default App
